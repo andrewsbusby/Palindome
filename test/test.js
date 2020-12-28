ba74bbc6908ad55496a73f035c1d0e50681b63f7
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2020-12-28 01:24:25 -0700

    created and added test and test.js

---
+++ test/test.js
@@ -0,0 +1,15 @@
+let assert = require("assert");
+let phrase = require("../index.js");
+
+describe("Phrase", function() {
+    describe("#palindrome", function() {
+        it("should return false for a non-palindrome", funciton() {
+            let nonPalindrome = new Phrase("apple");
+            assert(!nonPalindrome.palindrome());
+        });
+        it("should return true for a plain palindrome", function() {
+            let plaindPalindrome = new Phrase("racecar");
+            assert(plainPalindrome.palindrome());
+        });
+    });
+});
