f7faaf8ce50d428ed19b4957439fd4b4b7431da6
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-01-06 23:39:48 -0700

    fixes to test.js

--- test/test.js
+++ test/test.js
@@ -1,24 +1,28 @@
 let assert = require("assert");
-let phrase = require("../index.js");
+let Phrase = require("../index.js");
 
 describe("Phrase", function() {
-    describe("#palindrome", function() {
-        it("should return false for a non-palindrome", funciton() {
-            let nonPalindrome = new Phrase("apple"),
-            assert(!nonPalindrome.palindrome());
-        });
-        it("should return true for a plain palindrome", function() {
-            let plaindPalindrome = new Phrase("racecar");
-            assert(plainPalindrome.palindrome());
-        });
-        it("should return true for a mixed-case palindrome", function() {
-            let mixedCase = new phrase("RaceCar");
-            assert(mixedCase.palindrome());
-        });
-        it("should return true for a palindrome with punctuation", funtion() {
-            let punctuatedPalindrome = new Phrase("Madam, I'm Adam."),
-            assert(punctuatedPalindrome.palindrome());
-        });
-        
+
+  describe("#palindrome", function() {
+
+    it("should return false for a non-palindrome", function() {
+      let nonPalindrome = new Phrase("apple");
+      assert(!nonPalindrome.palindrome());
     });
+
+    it("should return true for a plain palindrome", function() {
+      let plainPalindrome = new Phrase("racecar");
+      assert(plainPalindrome.palindrome());
+    });
+
+    it("should return true for a mixed-case palindrome", function() {
+      let mixedCase = new Phrase("RaceCar");
+      assert(mixedCase.palindrome());
+    });
+
+    it("should return true for a palindrome with punctuation", function() {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert(punctuatedPalindrome.palindrome());
+    });
+  });
 });
